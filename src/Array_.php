d8bec0b6ed6539a34be5ae20efae8bec8381130a
Dmitry <inipel@ya.ru>
2024-08-29 13:43:46 +0300

    Update Array_.php

--- src/Array_.php
+++ src/Array_.php
@@ -211,7 +211,7 @@ class Array_
          return $this->only($array, $keys);
       }
       foreach ($array as $idx =>  $item) {
-         if (\is_array($item)) {
+         if ($this->accessible($item)) {
             $array[$idx] = $this->onlyNestedArray($item, $keys, ($depth - 1));
          }
       }
@@ -227,7 +227,7 @@ class Array_
          return $this->except($array, $keys);
       }
       foreach ($array as $idx =>  $item) {
-         if (\is_array($item)) {
+         if ($this->accessible($item)) {
             $array[$idx] = $this->exceptNestedArray($item, $keys, ($depth - 1));
          }
       }
