fc2a0a87d09555c6f8c86f8571d2385d6ef4b8a5
Dmitry <inipel@ya.ru>
2024-09-17 15:47:47 +0300

    splitIntoChunks fix

--- src/Array_.php
+++ src/Array_.php
@@ -452,10 +452,28 @@ class Array_
     * @param array<K,T> $array
     * @return array<int,array<K,T>>
     */
-   function splitIntoChunks(array $array, int $chunks, bool $preserve_keys = false): array
+   function splitIntoChunks(array $array, int $chunks, bool $preserve_keys = false, bool $remove_empty_chunks = false): array
    {
       if (!$array || $chunks === 0) return [];
-      return \array_chunk($array, \ceil(\sizeof($array) / \abs($chunks)), $preserve_keys);
+      // return \array_chunk($array, \ceil(\sizeof($array) / \abs($chunks)), $preserve_keys);
+
+      $i = 0;
+      $result = \array_fill(0, \abs($chunks), []);
+      foreach ($array as $key => $value) {
+
+         if ($preserve_keys) $result[$i][$key] = $value;
+         else $result[$i][] = $value;
+
+         $i++;
+
+         if (!isset($result[$i])) $i = 0;
+      }
+
+      if ($remove_empty_chunks) {
+         $result = \array_filter($result, static fn($sub_arr) => (bool)$sub_arr);
+      }
+
+      return $result;
    }
 
    /**
